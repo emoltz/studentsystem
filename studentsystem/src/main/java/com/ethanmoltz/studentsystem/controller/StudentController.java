325aebf9886f48dedf4a3d7fa4c8d280de0f6a56
emoltz <es5541@nyu.edu>
2022-12-17 17:24:20 -0500

    backend completed

--- studentsystem/src/main/java/com/ethanmoltz/studentsystem/controller/StudentController.java
+++ studentsystem/src/main/java/com/ethanmoltz/studentsystem/controller/StudentController.java
@@ -4,10 +4,8 @@ package com.ethanmoltz.studentsystem.controller;
 import com.ethanmoltz.studentsystem.model.Student;
 import com.ethanmoltz.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -19,5 +17,10 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New Student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
