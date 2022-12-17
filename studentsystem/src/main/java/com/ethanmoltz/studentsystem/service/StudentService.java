7830446683b2e5b64dc6269dbe956c706e8c8751
emoltz <es5541@nyu.edu>
2022-12-17 16:57:24 -0500

    autowired fix

--- studentsystem/src/main/java/com/ethanmoltz/studentsystem/service/StudentService.java
+++ studentsystem/src/main/java/com/ethanmoltz/studentsystem/service/StudentService.java
@@ -1,4 +1,10 @@
 package com.ethanmoltz.studentsystem.service;
 
+import com.ethanmoltz.studentsystem.model.Student;
+
 public class StudentService {
+    public Student saveStudent(Student student){
+
+        return student;
+    }
 }
--- studentsystem/src/main/java/com/ethanmoltz/studentsystem/service/StudentServiceImplementation.java
+++ studentsystem/src/main/java/com/ethanmoltz/studentsystem/service/StudentServiceImplementation.java
@@ -1,4 +1,18 @@
 package com.ethanmoltz.studentsystem.service;
 
-public class StudentServiceImplementation {
+import com.ethanmoltz.studentsystem.model.Student;
+import com.ethanmoltz.studentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentServiceImplementation extends StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
